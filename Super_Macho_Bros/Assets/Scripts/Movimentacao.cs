7d6843b788c86e4769a09ba38a8591fe70ca0dd3
Davi9990 <162383055+Davi9990@users.noreply.github.com>
2024-07-29 21:13:49 -0300

    Atualização_Pequena

--- Super_Macho_Bros/Assets/Scripts/Movimentacao.cs
+++ Super_Macho_Bros/Assets/Scripts/Movimentacao.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 public class Movimentacao : MonoBehaviour
 {
-    Rigidbody2D rbPlayer;
-    [SerializeField] float speed = 5f;
-    [SerializeField] float jumpForce = 15f;
-    [SerializeField] bool isJump;
-    [SerializeField] bool inFloor = true;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask groundLayer;
+    private Rigidbody2D rbPlayer;
+    private Animator animator;
+
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 15f;
+    [SerializeField] private bool isJump;
+    [SerializeField] private bool inFloor = true;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
     private void Awake()
     {
         rbPlayer = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         inFloor = Physics2D.Linecast(transform.position, groundCheck.position, groundLayer);
         Debug.DrawLine(transform.position, groundCheck.position, Color.blue);
+
+        // Atualiza os parâmetros do Animator
+        animator.SetBool("IsGrounded", inFloor);
+        animator.SetFloat("Speed", Mathf.Abs(rbPlayer.velocity.x));
 
         if (Input.GetButtonDown("Jump") && inFloor)
             isJump = true;
@@ -31,7 +38,7 @@ public class Movimentacao : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-       JumpPlayer();
+        JumpPlayer();
     }
 
     void Move()
@@ -39,22 +46,25 @@ public class Movimentacao : MonoBehaviour
         float xMove = Input.GetAxis("Horizontal");
         rbPlayer.velocity = new Vector2(xMove * speed, rbPlayer.velocity.y);
 
-        if(xMove > 0)
+        if (xMove > 0)
         {
-            transform.eulerAngles = new Vector2(0,0);
+            transform.eulerAngles = new Vector2(0, 0);
         }
-        else if(xMove < 0)
+        else if (xMove < 0)
         {
-            transform.eulerAngles = new Vector2 (0,180);
+            transform.eulerAngles = new Vector2(0, 180);
         }
     }
 
     void JumpPlayer()
     {
-        if(isJump)
+        if (isJump)
         {
             rbPlayer.velocity = Vector2.up * jumpForce;
-            isJump =  false;
+            isJump = false;
         }
+
+        // Atualiza o parâmetro IsJumping do Animator
+        animator.SetBool("IsJumping", !inFloor);
     }
 }
