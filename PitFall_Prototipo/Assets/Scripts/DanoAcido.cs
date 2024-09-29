2c7858cab4c623150dad84a1aef323367786b00b
Davi9990 <162383055+Davi9990@users.noreply.github.com>
2024-09-29 20:25:55 -0300

    PitFall_Maromba_Prototipo_V1.2

---
+++ PitFall_Prototipo/Assets/Scripts/DanoAcido.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanoAcido : MonoBehaviour
+{
+    public SistemaDeVida Vida;
+    public int damage = 1;
+    public float NextTimeAttack;
+    public bool PodeAtacar;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player") && PodeAtacar)
+        {
+            AplicarDano(collision);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && PodeAtacar)
+        {
+            AplicarDano(collision);
+        }
+    }
+
+    public void AplicarDano(Collision2D collision)
+    {
+        SistemaDeVida vida = collision.gameObject.GetComponent<SistemaDeVida>();
+        
+        if (vida != null)
+        {
+            vida.vida -= damage;
+            PodeAtacar = false;
+            Debug.Log("Ataque aplicado, cooldawn Iniciado.");
+            StartCoroutine (RecarregarAtaque());
+        }
+        else
+        {
+            Debug.Log("Componente Sistemadevida não encontrado no jogador");
+        }
+    }
+
+    IEnumerator RecarregarAtaque()
+    {
+        yield return new WaitForSeconds(NextTimeAttack);
+        PodeAtacar = true;
+        Debug.Log("Cooldawn de ataque finalizado");
+    }
+}
