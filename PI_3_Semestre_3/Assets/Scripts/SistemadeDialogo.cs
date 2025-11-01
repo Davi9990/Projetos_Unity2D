5d8923451d25df33ac0603e203a9ef16b90a3e74
Davi9990 <162383055+Davi9990@users.noreply.github.com>
2025-11-01 09:52:34 -0300

    ♻️ : Modificações nos script de missões

--- PI_3_Semestre_3/Assets/Scripts/SistemadeDialogo.cs
+++ PI_3_Semestre_3/Assets/Scripts/SistemadeDialogo.cs
@@ -2,7 +2,12 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class LinhaDialogo
